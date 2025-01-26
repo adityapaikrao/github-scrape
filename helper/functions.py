ee9814dd2de25dc9dcfad918bfb0e2544930276a
adityapaikrao <paikrao.aditya@gmail.com>
2025-01-26 14:03:22 -0500

    created preliminary skeleton for the script

---
+++ helper/functions.py
@@ -0,0 +1,8 @@
+def scrape_issues(repo):
+    """
+    Scrapes closed Issues from the given repository and returns data in a DataFrame
+    """
+    df = None
+
+
+    return df
---
+++ scrape.py
@@ -0,0 +1,24 @@
+import argparse
+from github import Auth, Github
+from helper.credentials_secret import GITHUB_TOKEN # replace credentials_secret to credentials
+from helper.functions import scrape_issues
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(prog='github-scrape')
+    parser.add_argument('--repo', help='repository to scrape from', default='tensorflow/tensorflow')
+    parser.add_argument('--pat', help='your github personal access token', default=GITHUB_TOKEN)
+    args = parser.parse_args()
+
+    repo = args.repo
+    token = args.pat
+
+    auth = Auth.Token(token)
+    g = Github(auth=auth)
+
+    repo = g.get_repo('tensorflow/tensorflow')
+    print(repo.name)
+
+    issues_df = scrape_issues(repo)
+
+    g.close()
