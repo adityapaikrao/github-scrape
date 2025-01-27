8d2d164d389138cc5be35a5ff87723ba51f1dad9
adityapaikrao <paikrao.aditya@gmail.com>
2025-01-26 21:48:09 -0500

    added inital implementation for scraping closed issues

--- helper/functions.py
+++ helper/functions.py
@@ -1,8 +1,83 @@
-def scrape_issues(repo):
+import pandas as pd
+
+def remove_quoted_comments(text):
+    """
+    Remove quoted comments in replies
+    """
+    lines = text.splitlines()
+    useful_lines = [line for line in lines if not line.strip().startswith('>')]
+
+    return "\n".join(useful_lines).strip()
+
+def process_comment(comment_list, issue_id, issue_creator, issue_assignees):
+    """
+    Processes each comment to be more readable
+    """
+    comments = []
+    comment_thread = ''
+
+    for comment in comment_list:
+        if comment.user.login == 'google-ml-butler[bot]': # only comments feedback URLs, skip
+            continue
+        
+        # Add possibly helpful role of authors in the thread
+        author = comment.user.login
+        if author == issue_creator:
+            author += " (Issue Creator)"
+
+        elif author in issue_assignees:
+            author += " (Assginee)"
+
+        date = comment.created_at.strftime("%Y-%m-%d %H:%M:%S %Z")
+        # convert instances of'> {prev_comment} \n {current comment}' to '{current comment}'
+        body = remove_quoted_comments(comment.body) 
+
+        comment_thread += f'{author} on ({date}): {body}' +'\n\n'
+        comments.append(
+            {
+                'comment_id':comment.id, 
+                "issue_id":issue_id, 
+                "author":comment.user.login,
+                "body":comment.body.strip(),
+                "created_at":comment.created_at
+                })
+    return comments, comment_thread
+
+def fetch_issue(issue):
+    """
+    Processes data for a single issue and returns it in a dictionary
+    """
+    if issue.pull_request:
+        return None
+    
+    assignee_list = [user.login for user in issue.assignees]
+    comments, comment_thread = process_comment(issue.get_comments(), issue.id, issue.user.login, assignee_list)
+    labels = issue.labels
+    
+    return  {
+            "id": issue.id,
+            "type": "issue",
+            "title": issue.title,
+            "author": issue.user.login,
+            # "assignees":issue.assignees,
+            "created_at": issue.created_at,
+            "closed_at": issue.closed_at,
+            "url": issue.html_url,
+            "labels": [(label.name, label.description) for label in labels],
+            "comments_list": comments,
+            "comment_thread":comment_thread
+        }
+
+def scrape_issues(repo, num=100, state='closed'):
     """
     Scrapes closed Issues from the given repository and returns data in a DataFrame
     """
-    df = None
+    
+    issues = []
+    for issue in repo.get_issues(state=state)[:num]:
+        result = fetch_issue(issue)
+        if result:
+            issues.append(result)
 
-
+    df = pd.DataFrame(issues)
     return df
--- scrape.py
+++ scrape.py
@@ -5,6 +5,7 @@ from helper.functions import scrape_issues
 
 if __name__ == "__main__":
 
+    # parse arguments 
     parser = argparse.ArgumentParser(prog='github-scrape')
     parser.add_argument('--repo', help='repository to scrape from', default='tensorflow/tensorflow')
     parser.add_argument('--pat', help='your github personal access token', default=GITHUB_TOKEN)
@@ -13,12 +14,15 @@ if __name__ == "__main__":
     repo = args.repo
     token = args.pat
 
+    # Authorise token
     auth = Auth.Token(token)
     g = Github(auth=auth)
 
     repo = g.get_repo('tensorflow/tensorflow')
     print(repo.name)
 
+    # scrape issues
     issues_df = scrape_issues(repo)
+    issues_df.to_csv('test.csv', index=False)
 
     g.close()
